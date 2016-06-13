97c28b7688702ad84f18e879e45fa11ebd8619af
gittyotter42 <josephinesiri@gmail.com>
2016-06-13 18:13:34 +0000

    Delete commented out calendar

--- bootstrap_v1/calendar.php
+++ bootstrap_v1/calendar.php
@@ -4,9 +4,6 @@
         <div class="col-lg-12">
             <div class = "white-panel">
                 <div align ="center">
-                    <!--<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ff6600&amp;
-                    src=thenookroom.com_2a8ub1urkc1o7cu1gs07nf7kd8%40group.calendar.google.com&amp;color=%23B1365F&amp;ctz=America%2FLos_Angeles" 
-                style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>   -->
                     <h2>Calendar</h2>
                     <div class = "responsiveCalandMap">
                         <iframe src="https://calendar.google.com/calendar/embed?showPrint=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=thenookroom.com_2a8ub1urkc1o7cu1gs07nf7kd8%40group.calendar.google.com&amp;color=%23B1365F&amp;ctz=America%2FLos_Angeles"
