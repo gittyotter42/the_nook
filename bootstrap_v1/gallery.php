ee6e32786223314e2677e2873c5b2bd1c564135a
gittyotter42 <josephinesiri@gmail.com>
2016-06-13 22:54:43 +0000

    Copy index.php carousel to gallery.php

--- bootstrap_v1/gallery.php
+++ bootstrap_v1/gallery.php
@@ -8,42 +8,132 @@
       <div class="col-lg-8">
         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
           <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
             <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
             <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
             <li data-target="#carousel-example-generic" data-slide-to="3" class=""></li>
             <li data-target="#carousel-example-generic" data-slide-to="4" class=""></li>
             <li data-target="#carousel-example-generic" data-slide-to="5" class=""></li>
             <li data-target="#carousel-example-generic" data-slide-to="6" class=""></li>
-            <li data-target="#carousel-example-generic" data-slide-to="7" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="7" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="8" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="9" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="10" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="11" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="12" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="13" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="14" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="15" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="16" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="17" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="18" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="19" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="20" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="21" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="22" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="23" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="24" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="25" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="26" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="27" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="28" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="29" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="30" class=""></li>
           </ol>
+          <!-- List of contents -->
           <div class="carousel-inner" role="listbox">
-            <div class="item">
+            <div class="item active">
               <img class="img-thumbnail" src="assets/images/books_740.jpg" alt="Books!" >
-            </div>
-            <div class="item active">
-              <img class="img-thumbnail" src="assets/images/cars_740.jpg" alt="Cars!" >
-            </div>
-            <div class="item active">
-              <img class="img-thumbnail" src="assets/images/books.jpg" alt="Cars!" >
-            </div>
-            <div class="item">
-              <img class="img-thumbnail" src="assets/images/nook_740.jpg" alt="The Nook" >
-            </div>
-            <div class="item">
-              <img class="img-thumbnail" src="assets/images/slides_740.jpg" alt="The Nook" >
-            </div>
-            <div class="item">
-              <img class="img-thumbnail" src="assets/images/ballpit.jpg" alt="The Nook" >
             </div>
             <div class="item">
               <img class="img-thumbnail" src="assets/images/nookplayground.jpg" alt="The Nook" >
             </div>
             <div class="item">
-              <img class="img-thumbnail" src="assets/images/readingnook.jpg" alt="The Nook" >
+              <img class="img-thumbnail" src="assets/images/viewingarea.jpg" alt="The Nook" >
             </div>
             <div class="item">
-              <img class="img-thumbnail" src="assets/images/viewingarea.jpg" alt="The Nook" >
+              <img class="img-thumbnail" src="assets/images/740books.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/959books.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02379.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02380.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02381.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02389.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02391.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02394.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02398.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02399.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02402.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02406.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02412.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02414.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02416.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02419.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02421.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02423.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02425.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02438.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02442.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02447.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02463.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02470.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02494.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02498.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02520.jpg" alt="The Nook" >
+            </div>
+            <div class="item">
+              <img class="img-thumbnail" src="assets/images/DSC02522.jpg" alt="The Nook" >
             </div>
           </div>
           <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
